a4310f9e81ceecbedb30f06db196602be550da73
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2023-12-13 18:20:39 +0000

    Removed World Clock

--- callcenter/src/App.js
+++ callcenter/src/App.js
@@ -1,14 +1,12 @@
 // App.js
 import React from 'react';
 import CallCenter from './CallCenter';
-import Clocks from './Clocks';
 import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Clocks />
       <CallCenter />
     </div>
   );
