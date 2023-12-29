6d2ed37232e8f4ca65243cf39bd27dffc2f62a51
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2023-12-29 00:54:21 +0000

    update

---
+++ lottery/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RandomNumberGenerator from './RandomNumberGenerator';
+
+const App = () => {
+  return (
+    <div>
+      <RandomNumberGenerator />
+    </div>
+  );
+};
+
+export default App;
