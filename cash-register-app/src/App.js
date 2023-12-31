a25261f80208e28a264fe5e2674c08048d13ab30
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2023-12-31 04:11:08 +0000

    update 7

---
+++ cash-register-app/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import CashRegister from './CashRegister';
+
+function App() {
+  return (
+    <div className="App">
+      <CashRegister />
+    </div>
+  );
+}
+
+export default App;
