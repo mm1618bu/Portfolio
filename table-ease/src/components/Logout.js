850b2720e284e7698717b61e75161e0715cc309d
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2024-06-27 00:13:25 +0000

    Logout Page

---
+++ table-ease/src/components/Logout.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+export default function Logout() {
+    return (
+        <div>
+            <h1>Logout</h1>
+            <p>You have been successfully logged out.</p>
+        </div>
+    );
+}
